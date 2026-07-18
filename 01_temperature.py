36a7699d8c1490cb6052d338639f6d93534ea25f
Aayuraj3006 <ashutosh981209@gmail.com>
2026-07-18 12:51:39 +0530

    Add initial temperature calculation and version display scripts

---
+++ 01_temperature.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+temperature_celsius = np.array([0, 20, 37, 100])
+average_temperature = np.mean(temperature_celsius)
+print("Average Temperature (Celsius):", average_temperature)
