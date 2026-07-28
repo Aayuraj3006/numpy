b161619c913427c97bb77099ff7f0c2a0b5e9f25
Aayuraj3006 <ashutosh981209@gmail.com>
2026-07-28 15:48:22 +0530

    Add array creation and properties display scripts for shape and conversion

---
+++ array/converstion.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr_warehouse = np.array([[1, 2, 3], [4, 5, 6]])
+print("Array works:", arr_warehouse.shape)
+print("Array works:", arr_warehouse.dtype)
+int_arr = arr_warehouse.astype(int)
+print("Array warehouse:", int_arr)
+print("Array works:", int_arr.dtype)
---
+++ array/shape.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr_warehouse = np.array([[1, 2, 3], [4, 5, 6]])
+print("Array works:", arr_warehouse.shape)
+print("Array works:", arr_warehouse.size)
+
+print("Array works:", arr_warehouse.ndim)
+print("Array works:", arr_warehouse.dtype)
+print("Array works:", arr_warehouse.itemsize)
+
+print("Array works:", arr_warehouse.dtype)
+
