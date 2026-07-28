6bbb954d0c74e5a696eebf21085dd9f48cb0fa27
Aayuraj3006 <ashutosh981209@gmail.com>
2026-07-28 15:57:09 +0530

    Add aggregation functions for sum, mean, median, std, var, min, and max

---
+++ aggregation/functions.py
@@ -0,0 +1,21 @@
+import numpy as np 
+arr_first = np.sum([[1, 2, 3], [4, 5, 6]])
+print("Array sum:", arr_first)
+
+arr_first= np.mean([[1, 2, 3], [4, 5, 6]])
+print("Array mean:", arr_first)
+
+arr_first= np.median([[1, 2, 3], [4, 5, 6]])
+print("Array median:", arr_first)
+
+arr_first= np.std([[1, 2, 3], [4, 5, 6]])
+print("Array standard deviation:", arr_first)
+
+arr_first= np.var([[1, 2, 3], [4, 5, 6]])
+print("Array variance:", arr_first)
+
+arr_first= np.min([[1, 2, 3], [4, 5, 6]])
+print("Array minimum:", arr_first)
+
+arr_first= np.max([[1, 2, 3], [4, 5, 6]])
+print("Array maximum:", arr_first)
